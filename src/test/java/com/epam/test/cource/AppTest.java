c0f57376e4a3d84ff4d4eb05d8f6d6ff818be8b4
xalf <alx287@gmail.com>
2018-02-14 17:40:59 +0300

    Test for DB connection task

--- src/test/java/com/epam/test/cource/AppTest.java
+++ src/test/java/com/epam/test/cource/AppTest.java
@@ -1,8 +1,30 @@
 package com.epam.test.cource;
 
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+    @Test
+    public void testMain() throws IOException, SQLException {
+        String[] args = null;
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        App.main(args);
+        outContent.flush();
+        String whatWasPrinted = new String(outContent.toByteArray());
+        String[] linesOfOutput = whatWasPrinted.split(System.getProperty("line.separator"));
+        assertEquals(14, linesOfOutput.length);
+        assertEquals("Hello World!", linesOfOutput[0]);
+    }
 
 }
